d8ea278639f3793b9f02babd44169a038e5ecc99
Erik Ostrom <code@erikostrom.com>
2026-03-20 22:56:28 +0000

    play a tone when a block has focus

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,4 +1,13 @@
-import { createSignal, type Component } from 'solid-js'
+import {
+  type Component,
+  createEffect,
+  createSignal,
+  on,
+  onCleanup,
+  onMount,
+} from 'solid-js'
+import { el } from '@elemaudio/core'
+import WebRenderer from '@elemaudio/web-renderer'
 
 type Block = number
 
@@ -18,6 +27,8 @@ type GridPosition = {
   column: number
 }
 
+const ctx = new AudioContext()
+
 const getGridPositionForBlock = (
   block: Block | null,
   grid: Grid,
@@ -34,7 +45,32 @@ const getGridPositionForBlock = (
 }
 
 export const Game: Component = () => {
-  const [grid, setGrid] = createSignal<Grid>(solvedGrid)
+  const [grid, setGrid] = createSignal(solvedGrid)
+  const [focused, setFocused] = createSignal<Block | null>(null)
+  const [core] = createSignal(new WebRenderer())
+
+  onMount(async () => {
+    let node = await core().initialize(ctx, {
+      numberOfInputs: 0,
+      numberOfOutputs: 1,
+      outputChannelCount: [2],
+    })
+    node.connect(ctx.destination)
+  })
+
+  onCleanup(async () => {
+    await ctx.close()
+  })
+
+  createEffect(
+    on(focused, async (block) => {
+      await ctx.resume()
+
+      const freq = 220 * 2 ** ((1 / 12) * ((block ?? 0) - 1))
+      const node = el.mul(el.const({ value: block ? 1 : 0 }), el.cycle(freq))
+      await core().render(node)
+    }),
+  )
 
   return (
     <div class={'grid grid-cols-4 gap-2'}>
@@ -45,6 +81,9 @@ export const Game: Component = () => {
           <button
             name="block"
             style={{ 'grid-row': row + 1, 'grid-column': column + 1 }}
+            class="hover:text-pink-500 focus:bg-pink-500"
+            onfocus={() => setFocused(block)}
+            onblur={() => setFocused(null)}
           >
             {block}
           </button>
